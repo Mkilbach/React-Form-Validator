4029fccda2ec9834e745f733a2b69aa3616b2748
MKilbach <mateusz.kilbach@avrio.pl>
2019-06-18 12:16:18 +0200

    input component finished

--- src/App.js
+++ src/App.js
@@ -5,7 +5,40 @@ import Input from "./components/FormValidator/Input";
 function App() {
   return (
     <div className="App">
-      <Input id={'id'} name={'name'} max={7} type={'checkbox'} defaultValue={3} title={'title'} rule={'select'} inputOptions={[[1, 2], 3, 4, [5, 6]]} required={true} errorText={'only numbers allowed'} />
+      <Input type={'radio'} id={'radio'} defaultValue={22} inputOptions={[[12, 22], 3, 4]} name={'test'} title={'radios'} />
+      <div>
+        <h2>
+          Input
+        </h2>
+        <div>
+          <Input id={'id'} name={'name'} max={7} checked={false} type={'checkbox'} title={'title'} rule={'select'} inputOptions={[[1, 2], 3, 4, [5, 6]]} required={true} errorText={'only numbers allowed'} />
+        </div>
+      </div>
+      <div>
+        <Input id={'input'} name={'name'} max={7} type={'input'} title={'input'} rule={'text'} required={false} min={2} errorText={'not allowed'} />
+      </div>
+      <Input type={'select'} errorClass={'customerror'} labelClass={'customlabel'} rule={'select'} inputOptions={[[12, 22], 3, 4, [5, 6]]} name={'select'} required={true} title={'select'} placeholder={'select your option'} />
+
+      <Input name="wrapper" required={true} name={'test'}>
+        <h5>radio1</h5>
+        <label htmlFor="radio1">
+          <input type='radio' id='radio1' name='test' value={1} /> <span>radio1</span>
+        </label>
+        <div>
+          <h5>radio2</h5>
+          <label htmlFor="radio2">
+            <input type="radio" name="test" id="radio2" value={'aaa'} /> <span>radio2</span>
+          </label>
+        </div>
+        <div>
+          <div>
+            <h5>radio3</h5>
+            <label htmlFor="radio3">
+              <input type="radio" name="test" id="radio3" value={33} /> <span>radio2</span>
+            </label>
+          </div>
+        </div>
+      </Input>
     </div>
   );
 }
--- src/components/FormValidator/Input.js
+++ src/components/FormValidator/Input.js
@@ -1,30 +1,63 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 import validate from "./validation";
 
 import './input.scss'
 
 export default class Input extends Component {
+    static propTypes = {
+        // name: PropTypes.string.isRequired
+    }
 
     state = {
         validate: false,
         valid: false,
         errorText: '',
-        value: this.props.defaultValue,
+        value: this.props.children ? '' : (this.props.type === 'checkbox' ? this.props.checked : this.props.defaultValue),
         confirmValue: ''
     }
 
+    async componentDidMount() {
+        const { children } = this.props;
+        if (children) await this.recursiveSearchInputs(children);
+
+        const { value } = this.state;
+        if (value) this.validateField();
+    }
+
+    recursiveSearchInputs = children => {
+        (Array.isArray(children) ? children : [children]).forEach(async el => {
+            if (el.type === 'input' && el.props.defaultChecked) await this.setState({ value: el.props.value });
+            if (el.props && el.props.children) this.recursiveSearchInputs(el.props.children);
+        })
+    }
+
     handleChange = async e => {
-        await this.setState({ value: e.target.value });
+        const { type, onBeforeChange, onAfterChange } = this.props;
+        const { validate } = this.state;
+        const { checked, value } = e.target;
+        const newValue = type === 'checkbox' ? checked : value;
+
+        onBeforeChange && onBeforeChange();
+
+        if ((type === 'checkbox' || type === 'select' || type === 'radio' || e.target.type === 'radio') && !validate) await this.setState({ validate: !validate });
+        await this.setState({ value: newValue });
         this.validateField();
+
+        onAfterChange && onAfterChange();
     }
 
     onBlur = e => {
         const { validate } = this.state;
+        const { onBlur } = this.props;
+
         if (!validate) {
             this.setState({ validate: !validate });
             this.validateField();
         }
+
+        onBlur && onBlur();
     }
 
     onFocus = e => {
@@ -48,13 +81,14 @@ export default class Input extends Component {
     checkIfOptionalEmpty = () => {
         const { value, confirmValue } = this.state;
         const { required } = this.props;
-        return (!required && value.length === 0 && confirmValue.length === 0) ? true : false;
+
+        return (!required && (value === undefined || value.length === 0 || value === false) && (confirmValue === undefined || confirmValue.length === 0 || confirmValue === false)) ? true : false;
     }
 
     checkRequired = () => {
         const { value, confirmValue } = this.state;
         const { required, rule } = this.props;
-        if (!required || (required && value && value.length > 0 && ((rule === 'confirm' && confirmValue && confirmValue > 0) || rule !== 'confirm'))) {
+        if (!required || (required && value && (value.toString().length > 0 || value === true) && ((rule === 'confirm' && confirmValue && (confirmValue.toString().length > 0 || confirmValue === true)) || rule !== 'confirm'))) {
             this.setState({ errorText: '' })
             return true;
         } else {
@@ -66,7 +100,9 @@ export default class Input extends Component {
     validateMinLength = () => {
         const { min, minErrorText, rule } = this.props;
         const { value, confirmValue } = this.state;
-        if (!min || (value.length > min && ((rule === 'confirm' && confirmValue > min) || rule !== 'confirm'))) {
+        console.log(value, min);
+        
+        if (!min || (value.toString().length >= min && ((rule === 'confirm' && confirmValue.toString().length >= min) || rule !== 'confirm'))) {
             this.setState({ errorText: '' })
             return true;
         } else {
@@ -124,6 +160,7 @@ export default class Input extends Component {
                 maxLength={max}
                 className={'validatedInput__input' + (inputClass ? ' ' + inputClass : '')}
                 value={value}
+                checked={value}
                 onChange={this.handleChange}
                 onBlur={this.onBlur}
                 onFocus={this.onFocus}
@@ -133,9 +170,10 @@ export default class Input extends Component {
 
     generateRadios = () => {
         const { id, name, disabled, readonly, type, inputClass, inputOptions } = this.props;
+        const { value } = this.state;
 
         return (
-            <div onChange={this.handleChange}>
+            <div onChange={this.handleChange} defaultValue={value}>
                 {Array.isArray(inputOptions) && inputOptions.map((el, i) => {
                     const optionName = Array.isArray(el) ? el[0] : el;
                     const optionValue = Array.isArray(el) ? el[1] : el;
@@ -151,6 +189,7 @@ export default class Input extends Component {
                                 onBlur={this.onBlur}
                                 onFocus={this.onFocus}
                                 value={optionValue}
+                                defaultChecked={value === optionValue}
                             />
                             <span>{optionName}</span>
                         </label>
@@ -166,11 +205,11 @@ export default class Input extends Component {
 
         return (
             <select
+
                 id={id}
                 name={name}
                 disabled={disabled ? true : false}
                 readOnly={readonly ? true : false}
-                placeholder={placeholder}
                 type={type}
                 className={'validatedInput__input' + (inputClass ? ' ' + inputClass : '')}
                 value={value}
@@ -178,7 +217,7 @@ export default class Input extends Component {
                 onBlur={this.onBlur}
                 onFocus={this.onFocus}
             >
-                <option readOnly hidden></option>
+                <option readOnly hidden>{placeholder}</option>
                 {Array.isArray(inputOptions) && inputOptions.map((el, i) => {
                     const optionName = Array.isArray(el) ? el[0] : el;
                     const optionValue = Array.isArray(el) ? el[1] : el;
@@ -189,16 +228,24 @@ export default class Input extends Component {
     }
 
     render() {
-        const { id, title, type } = this.props;
+        const { id, title, type, disabled, errorClass, labelClass, children } = this.props;
         const { errorText, validate, valid } = this.state;
 
         return (
-            <label htmlFor={id} className={'validatedInput' + (validate && (valid ? ' success' : ' error'))}>
-                {title && type !== 'checkbox' && <p className={'validatedInput__title'}>{title}</p>}
-                {this.generateField()}
-                {title && type === 'checkbox' && <span className={'validatedInput__title'}>{title}</span>}
-                {validate && !valid && errorText && <p className={'validatedInput__error'}>{errorText}</p>}
-            </label>
+            children
+                ? (
+                    <div onChange={this.handleChange} onBlur={this.onBlur} onFocus={this.onFocus} className={'validatedInput' + (labelClass ? ' ' + labelClass : '') + (validate && !disabled && (valid ? ' success' : ' error'))}>
+                        {children}
+                        {validate && !valid && errorText && <p className={'validatedInput__error' + (errorClass ? ' ' + errorClass : '')}>{errorText}</p>}
+                    </div>
+                ) : (
+                    <label htmlFor={id} className={'validatedInput' + (labelClass ? ' ' + labelClass : '') + (validate && !disabled && (valid ? ' success' : ' error'))}>
+                        {title && type !== 'checkbox' && <p className={'validatedInput__title'}>{title}</p>}
+                        {this.generateField()}
+                        {title && type === 'checkbox' && <span className={'validatedInput__title'}>{title}</span>}
+                        {validate && !valid && errorText && <p className={'validatedInput__error' + (errorClass ? ' ' + errorClass : '')}>{errorText}</p>}
+                    </label>
+                )
         )
     }
 }
